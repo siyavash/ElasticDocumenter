947fd7c15f7140aa67b83c92e17259ea0945c117
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2017-08-28 19:26:00 +0430

    Create RequestingThread

--- src/main/java/ElasticDocument/ElasticDocumenter.java
+++ src/main/java/ElasticDocument/ElasticDocumenter.java
@@ -1,17 +1,11 @@
 package ElasticDocument;
 
-import com.google.gson.Gson;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.nio.entity.NStringEntity;
 import org.apache.log4j.Logger;
-import org.elasticsearch.client.Response;
-import org.elasticsearch.client.RestClient;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.concurrent.ArrayBlockingQueue;
 
@@ -20,9 +14,8 @@ public class ElasticDocumenter
     private PageInfoDataStore dataStore;
     private Logger logger = Logger.getLogger(Class.class.getName());
     private ArrayBlockingQueue<PageInfo> pageInfoArrayBlockingQueue = new ArrayBlockingQueue<>(10000);
-//    private int requestCount;
     private int iterateCount;
-    private static int REQUEST_THREAD_DOC_NUM = 15;
+    private static int REQUEST_THREAD_NUM = 2;
 
     public static void main(String[] args) throws IOException
     {
@@ -43,65 +36,23 @@ public class ElasticDocumenter
 
         try
         {
-            startSendingRequests(); //TODO close client
+            startSendingRequestsThread(); //TODO close client
         } catch (InterruptedException e)
         {
-            e.printStackTrace(); // TODO
+            e.printStackTrace(); //TODO
         }
     }
 
-    private void startSendingRequests() throws InterruptedException, IOException //TODO handle exceptions
+    private void startSendingRequestsThread() throws InterruptedException
     {
-        RestClient restClient = RestClient.builder(new HttpHost("slave", 9200, "http"),
-                new HttpHost("slave", 9201, "http")).build();
+        Thread slaveRequestingThread = new RequestingThread("slave", pageInfoArrayBlockingQueue);
+        Thread masterRequestingThread = new RequestingThread("master", pageInfoArrayBlockingQueue);
 
-        long t1 = 0;
+        slaveRequestingThread.start();
+        masterRequestingThread.start();
 
-        while (true)
-        {
-            ArrayList<PageInfo> pageInfos = new ArrayList<>();
-
-            for (int i = 0; i < REQUEST_THREAD_DOC_NUM; i++)
-            {
-                pageInfos.add(pageInfoArrayBlockingQueue.take());
-            }
-
-            t1 = System.currentTimeMillis();
-
-            String requestBody = createRequestBody(pageInfos);
-            HttpEntity putEntity = new NStringEntity(requestBody, ContentType.APPLICATION_JSON);
-            Response addingResponse = restClient.performRequest("POST", "_bulk", Collections.emptyMap(), putEntity);
-
-            t1 = System.currentTimeMillis() - t1;
-
-            logger.info("Request sent in " + t1 + " milli seconds");
-            writeURLToFile(pageInfos.get(4).getUrl());
-//            requestCount += 5;
-//
-//            if (requestCount % 100 == 0)
-//            {
-//                logger.info(requestCount + " request sent");
-//            }
-        }
-    }
-
-    private String createRequestBody(ArrayList<PageInfo> pageInfos)
-    {
-        StringBuilder finalRequest = new StringBuilder();
-        Gson gson = new Gson();
-
-        for (PageInfo pageInfo : pageInfos)
-        {
-            finalRequest.append("{ \"index\" : { \"_index\" : \"gagoole\", \"_type\" : \"page\"} }");
-            finalRequest.append("\n");
-
-            String request = gson.toJson(pageInfo, PageInfo.class);
-
-            finalRequest.append(request);
-            finalRequest.append("\n");
-        }
-
-        return finalRequest.toString();
+        slaveRequestingThread.join();
+        masterRequestingThread.join();
     }
 
     private void startIteratingThread() throws IOException //TODO handle exce[ptions
@@ -189,46 +140,6 @@ public class ElasticDocumenter
             } catch (IOException ex)
             {
                 ex.printStackTrace();
-            }
-
-        }
-    }
-
-    private void writeURLToFile(String url)
-    {
-        BufferedWriter bw = null;
-        FileWriter fw = null;
-
-        try
-        {
-            fw = new FileWriter("lastUrlName.txt");
-            bw = new BufferedWriter(fw);
-            bw.write(url);
-
-//            logger.info("UrlName file updated");
-
-        } catch (IOException e)
-        {
-
-            logger.warn("failed to update UrlName file");
-
-        } finally
-        {
-
-            try
-            {
-
-                if (bw != null)
-                    bw.close();
-
-                if (fw != null)
-                    fw.close();
-
-            } catch (IOException ex)
-            {
-
-                ex.printStackTrace();
-
             }
 
         }
---
+++ src/main/java/ElasticDocument/RequestingThread.java
@@ -0,0 +1,132 @@
+package ElasticDocument;
+
+import com.google.gson.Gson;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.nio.entity.NStringEntity;
+import org.apache.log4j.Logger;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class RequestingThread extends Thread
+{
+    private RestClient restClient; //TODO implement it in a way to be able to close it
+    private static int REQUEST_THREAD_DOC_NUM = 15;
+    private Logger logger = Logger.getLogger(Class.class.getName());
+    private ArrayBlockingQueue<PageInfo> pageInfoArrayBlockingQueue;
+
+
+    public RequestingThread(String host, ArrayBlockingQueue<PageInfo> pageInfoArrayBlockingQueue)
+    {
+        restClient = RestClient.builder(new HttpHost(host, 9200, "http"),
+                new HttpHost(host, 9201, "http")).build();
+
+        this.pageInfoArrayBlockingQueue = pageInfoArrayBlockingQueue;
+    }
+
+    @Override
+    public void run()
+    {
+        long t1;
+
+        while (true)
+        {
+            ArrayList<PageInfo> pageInfos = new ArrayList<>();
+
+            for (int i = 0; i < REQUEST_THREAD_DOC_NUM; i++)
+            {
+                try
+                {
+                    pageInfos.add(pageInfoArrayBlockingQueue.take());
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace(); //TODO
+                }
+            }
+
+            t1 = System.currentTimeMillis();
+
+            String requestBody = createRequestBody(pageInfos);
+            HttpEntity putEntity = new NStringEntity(requestBody, ContentType.APPLICATION_JSON);
+            try
+            {
+                Response addingResponse = restClient.performRequest("POST", "_bulk", Collections.emptyMap(), putEntity);
+            } catch (IOException e)
+            {
+                e.printStackTrace(); //TODO
+            }
+
+            t1 = System.currentTimeMillis() - t1;
+
+            logger.info("Request sent in " + t1 + " milli seconds");
+            writeURLToFile(pageInfos.get(4).getUrl());
+        }
+    }
+
+    private void writeURLToFile(String url)
+    {
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
+        try
+        {
+            fw = new FileWriter("lastUrlName.txt");
+            bw = new BufferedWriter(fw);
+            bw.write(url);
+
+//            logger.info("UrlName file updated");
+
+        } catch (IOException e)
+        {
+
+            logger.warn("failed to update UrlName file");
+
+        } finally
+        {
+
+            try
+            {
+
+                if (bw != null)
+                    bw.close();
+
+                if (fw != null)
+                    fw.close();
+
+            } catch (IOException ex)
+            {
+
+                ex.printStackTrace();
+
+            }
+
+        }
+    }
+
+    private String createRequestBody(ArrayList<PageInfo> pageInfos)
+    {
+        StringBuilder finalRequest = new StringBuilder();
+        Gson gson = new Gson();
+
+        for (PageInfo pageInfo : pageInfos)
+        {
+            finalRequest.append("{ \"index\" : { \"_index\" : \"gagoole\", \"_type\" : \"page\"} }");
+            finalRequest.append("\n");
+
+            String request = gson.toJson(pageInfo, PageInfo.class);
+
+            finalRequest.append(request);
+            finalRequest.append("\n");
+        }
+
+        return finalRequest.toString();
+    }
+}
