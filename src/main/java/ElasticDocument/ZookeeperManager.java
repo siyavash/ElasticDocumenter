bb9cfb637b46c96781e78c24ad0f609867f61c71
Ali Ehteshami <ali.ehteshami7@gmail.com>
2017-09-12 01:27:28 +0430

    rename FileSaver to Zookeepermanager and use apache curator in it for do the works

---
+++ src/main/java/ElasticDocument/ZookeeperManager.java
@@ -0,0 +1,75 @@
+package ElasticDocument;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ZookeeperManager {
+    private CuratorFramework client;
+    private String path;
+    private String ipAdress;
+
+    public ZookeeperManager(String hosts, String path, String ipAddress) throws IOException, InterruptedException {
+        this.path = path;
+        this.ipAdress = ipAddress;
+        client = CuratorFrameworkFactory.newClient(hosts, new ExponentialBackoffRetry(1000, 3));
+        client.start();
+    }
+
+    public void close() {
+        client.close();
+    }
+
+    public void makeEphemeral() throws Exception {
+        client.create().withMode(CreateMode.EPHEMERAL).forPath(path + "/servers/" + ipAdress + "/isAlive");
+    }
+
+    public long getNewTime(long timeSteps) throws Exception {
+        InterProcessMutex lock = new InterProcessMutex(client, path + "/servers/" + ipAdress + "time/lock");
+        lock.acquire();
+
+        long time;
+
+        time = Long.parseLong(new String(client.getData().forPath(path + "/servers/" + ipAdress + "/time")));
+
+        lock.release();
+
+        if (time != -1)
+            return time;
+
+        List<String> children = client.getChildren().forPath(path + "/servers/");
+        for (String child : children) {
+            if (client.checkExists().forPath(path + "/servers/" + child + "/isAlive") == null) {
+                lock = new InterProcessMutex(client, path + "/servers/" + child + "time/lock");
+                lock.acquire();
+                time = Long.parseLong(new String(client.getData().forPath(path + "/servers/" + child + "/time")));
+                if (time != -1) {
+                    deleteTime(child);
+                    lock.release();
+                    return time;
+                }
+                lock.release();
+            }
+        }
+
+        lock = new InterProcessMutex(client, path + "/nextTime/lock");
+        lock.acquire();
+        time = Long.parseLong(new String(client.getData().forPath(path + "/nextTime")));
+        client.setData().forPath(path + "/nextTime", Long.toString(time + timeSteps).getBytes());
+        lock.release();
+        return time;
+    }
+
+    public void deleteTime() throws Exception {
+        client.setData().forPath(path + "/servers/" + ipAdress + "/time", "-1".getBytes());
+    }
+
+    public void deleteTime(String child) throws Exception {
+        client.setData().forPath(path + "/servers/" + child + "/time", "-1".getBytes());
+    }
+}
